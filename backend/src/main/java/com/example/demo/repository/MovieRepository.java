23ba0fe75b7df70aa04c1be28ed4185b96eced37
belladelgado <belladelgado@comcast.net>
2026-02-21 11:26:09 -0500

    change dependencies to update DB to mySQL

--- backend/src/main/java/com/example/demo/repository/MovieRepository.java
+++ backend/src/main/java/com/example/demo/repository/MovieRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo.repository;
-
 import com.example.demo.model.Movie;
-import org.springframework.data.mongodb.repository.MongoRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface MovieRepository extends MongoRepository<Movie, String> {
+public interface MovieRepository extends JpaRepository<Movie, String> {
     List<Movie> findByStatus(String status);
     List<Movie> findByTitleContainingIgnoreCase(String title);
     List<Movie> findByCategory(String category);
