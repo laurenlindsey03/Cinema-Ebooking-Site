90c21351f13d7ceee863a8e48cc22bb1bada0231
belladelgado <belladelgado@comcast.net>
2026-03-18 15:27:36 -0400

    repository and controller for users class outlined

---
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Movie;
+import com.example.demo.model.Card;
+import com.example.demo.model.User;
+import org.springframework.web.bind.annotation.*;
+
+
+// PLACEHOLDER
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
+public class UserController {
+    
+}
---
+++ backend/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Showtime;
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// PLACEHOLDER
+public class UserRepository extends JpaRepository<User, Long>{
+    
+}
