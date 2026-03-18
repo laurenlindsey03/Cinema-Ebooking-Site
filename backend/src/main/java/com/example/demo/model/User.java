6d86d852de257635fed2df3dc39e1eec110a1d16
belladelgado <belladelgado@comcast.net>
2026-03-18 15:23:07 -0400

    user class created

---
+++ backend/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,89 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users") //PLACEHOLDER
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String password; 
+    
+    private String userStatus; 
+    private String role;   
+
+    public User() {
+    }
+    
+    public Long getId() { 
+        return id; 
+    }
+
+    public void setId(Long id) { 
+        this.id = id; 
+    }
+
+    public String getEmail() { 
+        return email; 
+    }
+
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+
+    public String getFirstName() { 
+        return firstName; 
+    }
+
+    public void setFirstName(String firstName) { 
+        this.firstName = firstName; 
+    }
+
+    public String getLastName() { 
+        return lastName; 
+    }
+
+    public void setLastName(String lastName) { 
+        this.lastName = lastName; 
+    }
+
+    public String getPhoneNumber() { 
+        return phoneNumber; 
+    }
+    
+    public void setPhoneNumber(String phoneNumber) { 
+        this.phoneNumber = phoneNumber; 
+    }
+
+    public String getPassword() { 
+        return password; 
+    }
+    
+    public void setPassword(String password) { 
+        this.password = password; 
+    }
+
+    public String getUserStatus() { 
+        return userStatus; 
+    }
+
+    public void setUserStatus(String userStatus) { 
+        this.userStatus = userStatus; 
+    }
+
+    public String getRole() { 
+        return role; 
+    }
+
+    public void setRole(String role) { 
+        this.role = role; 
+    }
+
+    
+}
